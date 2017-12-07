0f1612fb8f145007fbb3785037e1bb7f979214cf
qianghao <qiang.hao@wwu.edu>
2017-12-06 21:41:15 -0800

    fix img not loading bug

--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -1,39 +1,48 @@
 var express = require('express');
-var fortune = require('./lib/fortune.js');
 
 var app = express();
-app.set('port', process.env.PORT || 3000);
 
 // set up handlebars view engine
 var handlebars = require('express-handlebars')
-		.create({ defaultlayout: 'main' });
+	.create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
+var fortuneCookies = [
+	"Conquer your fears or they will conquer you.",
+	"Rivers need springs.",
+	"Do not fear what you don't know.",
+	"You will have a pleasant surprise.",
+	"Whenever possible, keep it simple.",
+];
 
 app.get('/', function(req, res) {
 	res.render('home');
 });
-
-app.get('/about', function(req, res) {
-	res.render('about', { fortune : fortune.getFortune() });
+app.get('/about', function(req,res){
+	var randomFortune = 
+		fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
+	res.render('about', { fortune: randomFortune });
 });
 
-// custom 404 page
-app.use(function(req, res) {
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
 	res.status(404);
 	res.render('404');
 });
 
-// custom 500 page
-app.use(function(req, res) {
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
 	console.error(err.stack);
 	res.status(500);
 	res.render('500');
 });
 
-app.listen(app.get('port'), function() {
-	console.log('Express started on http://localhost:' +
-		app.get('port') + '; press Ctrl-C to terminate.');
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' + 
+    app.get('port') + '; press Ctrl-C to terminate.' );
 });
