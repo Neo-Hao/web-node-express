47195b20c348d69e40bb607d6ad61f4777116bbb
qianghao <qiang.hao@wwu.edu>
2017-12-07 20:43:57 -0800

    refacotorize the project to work with Travic-C

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,12 +17,9 @@ module.exports = function(grunt){
 		jshint: {
 			app: ['meadowlark.js', 'public/js/**/*.js', 'lib/**/*.js'],
 			qa: ['Gruntfile.js', 'public/qa/**/*.js', 'qa/**/*.js'],
-		},
-		exec: {
-			linkchecker: { cmd: 'linkchecker http://localhost:3000' }
-		},
+		}
 	});	
 
 	// register tasks
-	grunt.registerTask('default', ['cafemocha','jshint','exec']);
+	grunt.registerTask('default', ['cafemocha','jshint']);
 };
